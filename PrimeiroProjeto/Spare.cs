395087b2e2d39334957ec0df7206e3b2b1afcad0
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-03-14 16:53:54 -0300

    Add project files.

---
+++ PrimeiroProjeto/Spare.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o nome do funcionário: ");
+        string nome = Console.ReadLine()!;
+        if (String.IsNullOrEmpty(nome) || double.TryParse(nome, out _) || nome.Any(char.IsDigit))
+        {
+            Console.WriteLine("O nome não pode estar vazio ou ser um número.");
+            return;
+        }
+
+        Console.Write("Digite o salário fixo do funcionário: ");
+        bool sucesso2 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double salario);
+
+        Console.Write("Quanto que ele vendeu? ");
+        bool sucesso3 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double vendas);
+
+        if (sucesso2 && sucesso3)
+        {
+            Console.WriteLine($"O funcionário {nome} tem um salário fixo de R${salario:F2}. Mas, tendo em vista que ele vendeu, em Reais, R${vendas:F2}, e que o funcionário tem 15% de comissão das vendas, seu salário final será de R${salario+vendas * 0.15:F2}.");
+        }
+        else
+        {
+            Console.WriteLine("Verifique se os dados do salário e das vendas estão válidos.");
+        }
+        Console.ReadLine();
+    }
+}
+
---
+++ SubProjeto2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SubProjeto2
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Quanto foi a compra?");
+            double com = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Quanto que foi dado ao caixa?");
+            double rec = double.Parse(Console.ReadLine()!);
+
+            double res = (rec - com);
+
+            double div = (res * (-1));
+            {
+
+            //if e else são condições
+
+                if (res >= 0)
+                {
+                    Console.WriteLine($"Seu troco é de R${res}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Você deve R${div}.");
+                }
+            }
+        }
+    }
+
+}
