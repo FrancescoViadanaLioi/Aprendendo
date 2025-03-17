f17bdedce0dcb7e570dce265c249286d9f7b2f78
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-03-17 17:16:03 -0300

    Exercício que calcula o Km/L de um veículo.

--- PrimeiroProjeto/Spare.cs
+++ PrimeiroProjeto/Spare.cs
@@ -4,29 +4,31 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Digite o nome do funcionário: ");
-        string nome = Console.ReadLine()!;
-        if (String.IsNullOrEmpty(nome) || double.TryParse(nome, out _) || nome.Any(char.IsDigit))
+        Console.Write("Quantos quilometros você rodou? ");
+        bool sucesso1 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double kmRodados);
+
+        if (!sucesso1 || kmRodados <= 0)
         {
-            Console.WriteLine("O nome não pode estar vazio ou ser um número.");
+            Console.WriteLine("Favor, verifique se o valor digitado é válido e maior do que 0.");
             return;
         }
 
-        Console.Write("Digite o salário fixo do funcionário: ");
-        bool sucesso2 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double salario);
-
-        Console.Write("Quanto que ele vendeu? ");
-        bool sucesso3 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double vendas);
-
-        if (sucesso2 && sucesso3)
-        {
-            Console.WriteLine($"O funcionário {nome} tem um salário fixo de R${salario:F2}. Mas, tendo em vista que ele vendeu, em Reais, R${vendas:F2}, e que o funcionário tem 15% de comissão das vendas, seu salário final será de R${salario+vendas * 0.15:F2}.");
-        }
         else
         {
-            Console.WriteLine("Verifique se os dados do salário e das vendas estão válidos.");
+            Console.Write("Quantos litros de combustível você consumiu? ");
+            bool sucesso2 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double litrosConsumidos);
+
+            if (!sucesso2 || litrosConsumidos <= 0)
+            {
+                Console.WriteLine("Favor, verifique se o valor digitado é válido e maior do que 0.");
+                return;
+            }
+            else
+            {
+                double consumoMedio = kmRodados / litrosConsumidos;
+                Console.WriteLine($"Seu consumo médio é de {consumoMedio.ToString("F2", CultureInfo.InvariantCulture)}km/l.");
+            }
         }
-        Console.ReadLine();
     }
 }
 
