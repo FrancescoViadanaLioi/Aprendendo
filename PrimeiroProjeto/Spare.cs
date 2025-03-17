961989a9385fcb36d2747c35d8f19c37836e0628
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-03-17 16:56:48 -0300

    Fiz um exercício de cálcuo de volume da esfera.

--- PrimeiroProjeto/Spare.cs
+++ PrimeiroProjeto/Spare.cs
@@ -1,32 +1,24 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Digite o nome do funcionário: ");
-        string nome = Console.ReadLine()!;
-        if (String.IsNullOrEmpty(nome) || double.TryParse(nome, out _) || nome.Any(char.IsDigit))
+       Console.Write("Qual é o raio da esfera? ");
+       bool sucesso1 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double raio);
+
+        if(!sucesso1 || raio<=0)
         {
-            Console.WriteLine("O nome não pode estar vazio ou ser um número.");
+            Console.WriteLine("Valor inválido. Por favor, inserir um número maior que zero.");
             return;
-        }
-
-        Console.Write("Digite o salário fixo do funcionário: ");
-        bool sucesso2 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double salario);
-
-        Console.Write("Quanto que ele vendeu? ");
-        bool sucesso3 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double vendas);
-
-        if (sucesso2 && sucesso3)
-        {
-            Console.WriteLine($"O funcionário {nome} tem um salário fixo de R${salario:F2}. Mas, tendo em vista que ele vendeu, em Reais, R${vendas:F2}, e que o funcionário tem 15% de comissão das vendas, seu salário final será de R${salario+vendas * 0.15:F2}.");
         }
         else
         {
-            Console.WriteLine("Verifique se os dados do salário e das vendas estão válidos.");
+            double vol = (4.0/3.0) * Math.PI * Math.Pow(raio, 3);
+            Console.WriteLine($"O volume de uma esfera de raio {raio.ToString("F2", CultureInfo.InvariantCulture)} é de {vol.ToString("F2", CultureInfo.InvariantCulture)}. ");
         }
         Console.ReadLine();
     }
-}
+    }
 
