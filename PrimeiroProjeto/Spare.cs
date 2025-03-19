54316092cbd96a4f9d561ad64c90a007fd2c23ee
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-03-19 14:52:53 -0300

    Alterei para else if

--- PrimeiroProjeto/Spare.cs
+++ PrimeiroProjeto/Spare.cs
@@ -4,16 +4,14 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Quantos quilometros você rodou? ");
+        Console.Write("Quantos quilômetros você rodou? ");
         bool sucesso1 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double kmRodados);
 
         if (!sucesso1 || kmRodados <= 0)
         {
             Console.WriteLine("Favor, verifique se o valor digitado é válido e maior do que 0.");
-            return;
         }
-
-        else
+        else if (sucesso1) // Verifica a entrada do segundo valor apenas se o primeiro foi válido
         {
             Console.Write("Quantos litros de combustível você consumiu? ");
             bool sucesso2 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double litrosConsumidos);
@@ -21,7 +19,6 @@ class Program
             if (!sucesso2 || litrosConsumidos <= 0)
             {
                 Console.WriteLine("Favor, verifique se o valor digitado é válido e maior do que 0.");
-                return;
             }
             else
             {
@@ -29,6 +26,6 @@ class Program
                 Console.WriteLine($"Seu consumo médio é de {consumoMedio.ToString("F2", CultureInfo.InvariantCulture)}km/l.");
             }
         }
+        Console.ReadLine();
     }
 }
-
