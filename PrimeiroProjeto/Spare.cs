9a4f187638c7c6e6890b77ecfce966512e7335ad
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-03-19 14:46:12 -0300

    Código que usa a fórmula do cálcuo de distância entre dois pontos.

--- PrimeiroProjeto/Spare.cs
+++ PrimeiroProjeto/Spare.cs
@@ -4,22 +4,26 @@ class Program
 {
     static void Main()
     {
-        double A, B, maior;
-        Console.Write("Digite dois valores separados por espaço: ");
-        string[] valores = Console.ReadLine()!.Split(' ');
+        double x1, x2, y1, y2, d;
+        Console.Write("Digite os valores de x1, x2, y1 e y2, respectivamente(separados por espaços, somente 4 entradas e somente números positivos): ");
+        string[] vetor = Console.ReadLine()!.Split(' ');
 
         if
-            (valores.Length !=2||!double.TryParse(valores[0], NumberStyles.Any, CultureInfo.InvariantCulture, out A) ||
-            !double.TryParse(valores[1], NumberStyles.Any, CultureInfo.InvariantCulture, out B))
+            (vetor.Length !=4 ||
+                !double.TryParse(vetor[0], NumberStyles.Any, CultureInfo.InvariantCulture, out x1) ||
+                !double.TryParse(vetor[1], NumberStyles.Any, CultureInfo.InvariantCulture, out x2) ||
+                !double.TryParse(vetor[2], NumberStyles.Any, CultureInfo.InvariantCulture, out y1) ||
+                !double.TryParse(vetor[3], NumberStyles.Any, CultureInfo.InvariantCulture, out y2))
         {
-            Console.WriteLine("Erro! Favor inserir 2 números. Espaços em branco e/ou carcteres não serão aceitos.");
+            Console.WriteLine("Digitação inválida. Favor verificar se há espaços, se há somente 4 entradas e se são números positivos.");
             return;
         }
         else
         {
-            maior = (A + B + Math.Abs(A - B)) / 2;
-            Console.WriteLine($"O maior valor é {maior}");
+            d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"A distância entre os pontos ({x1.ToString("F2",CultureInfo.InvariantCulture)};{y1.ToString("F2", CultureInfo.InvariantCulture)}) e");
+            Console.Write($"({x2.ToString("F2", CultureInfo.InvariantCulture)};{y2.ToString("F2", CultureInfo.InvariantCulture)}) é de {d.ToString("F2", CultureInfo.InvariantCulture)}");
         }
-        Console.ReadLine();
+            Console.ReadLine();
     }
 }
