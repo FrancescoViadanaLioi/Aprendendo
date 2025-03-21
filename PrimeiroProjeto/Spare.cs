0cb290fbe1e0d113c7431df711850378e33da34b
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-03-21 16:13:51 -0300

    Programa que coleta a entrada(segundos) e transforma os segundos em hora(s), minuto(s), e segundo(s).

--- PrimeiroProjeto/Spare.cs
+++ PrimeiroProjeto/Spare.cs
@@ -4,26 +4,21 @@ class Program
 {
     static void Main()
     {
-        double x1, x2, y1, y2, d;
-        Console.Write("Digite os valores de x1, x2, y1 e y2, respectivamente(separados por espaços, somente 4 entradas e somente números positivos): ");
-        string[] vetor = Console.ReadLine()!.Split(' ');
+        int N;
+        Console.Write("Escreva o número de segundos: ");
+        bool sucess = int.TryParse(Console.ReadLine(), out N);
 
-        if
-            (vetor.Length !=4 ||
-                !double.TryParse(vetor[0], NumberStyles.Any, CultureInfo.InvariantCulture, out x1) ||
-                !double.TryParse(vetor[1], NumberStyles.Any, CultureInfo.InvariantCulture, out x2) ||
-                !double.TryParse(vetor[2], NumberStyles.Any, CultureInfo.InvariantCulture, out y1) ||
-                !double.TryParse(vetor[3], NumberStyles.Any, CultureInfo.InvariantCulture, out y2))
+        if (!sucess || N < 0)
         {
-            Console.WriteLine("Digitação inválida. Favor verificar se há espaços, se há somente 4 entradas e se são números positivos.");
-            return;
+            Console.WriteLine("Dados inválidos, favor redigitar um número inteiro positivo.");
         }
         else
         {
-            d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.Write($"A distância entre os pontos ({x1.ToString("F2",CultureInfo.InvariantCulture)};{y1.ToString("F2", CultureInfo.InvariantCulture)}) e ");
-            Console.Write($"({x2.ToString("F2", CultureInfo.InvariantCulture)};{y2.ToString("F2", CultureInfo.InvariantCulture)}) é de {d.ToString("F2", CultureInfo.InvariantCulture)}");
+            int horas = N / 3600;
+            int minutos = (N % 3600) / 60;
+            int segundos = (N % 3600) % 60;
+            Console.WriteLine($"Em {N} segundos, há {horas} horas, {minutos} minutos e {segundos} segundos. ");
         }
-            Console.ReadLine();
+        Console.ReadLine();
     }
 }
