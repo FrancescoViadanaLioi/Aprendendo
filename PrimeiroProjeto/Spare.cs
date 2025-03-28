c30619cce5ce32e4b7a99afd025d6f7120911d16
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-03-28 16:02:41 -0300

    Sistema que vizualiza as notas de dois semestres, e o total da soma delas apresentará se o aluno foi aprovado ou não(o mínimo para aprovação é se a soma das notas for pelo menos 70).

--- PrimeiroProjeto/Spare.cs
+++ PrimeiroProjeto/Spare.cs
@@ -1,57 +1,47 @@
 ﻿using System;
 using System.Globalization;
 
-partial class Program
+class Program
 {
     static void Main()
     {
-        int D1 = 0, H1 = 0, M1 = 0, S1 = 0, D2 = 0, H2 = 0, M2 = 0, S2 = 0, DF = 0, HF = 0, MF = 0, SF = 0,inicio, fim, duracao, resto;
+        double N1, N2, NF;
 
-        Console.Write("Digite a data, hora, minuto e segundo de início do evento(separados por espaços): ");
-        string[] v = Console.ReadLine()!.Split(' ');
+        Console.Write("Digite a nota do primeiro semestre: ");
 
-        if (!int.TryParse(v[0], out D1) || D1<=0 ||
-            !int.TryParse(v[1], out H1) || H1<=0 || H1 >=24 ||
-            !int.TryParse(v[2], out M1) || M1<=0 || M1 >=60 ||
-            !int.TryParse(v[3], out S1) || S1<=0 || S1 >=60 ||
-            v.Length != 4)
+        if (!double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out N1) || N1 < 0 || N1 > 50)
         {
-            Console.WriteLine("Digitação inválida, favor verificar se há somente números inteiros positivos.");
+            Console.WriteLine("Valor inválido, favor redigitar.");
+            return;
         }
+
         else
         {
-            Console.Write("Digite a data, hora, minuto e segundo de término do evento(separados por espaços): ");
-            v = Console.ReadLine()!.Split(' ');
-
-            if (!int.TryParse(v[0], out D2) || D2 <=0 ||
-                !int.TryParse(v[1], out H2) || H2 <=0 || H2 >=24 ||
-                !int.TryParse(v[2], out M2) || M2 <=0 || M2 >=60 ||
-                !int.TryParse(v[3], out S2) || S2 <=0 || S2 >=60 ||
-                v.Length !=4)
-            {
-                Console.WriteLine("Digitação inválida, favor verificar se há somente números inteiros positivos.");
-            }
-            else
-            {
-                inicio = (D1 - 1) * 24 * 60 * 60 + (H1 * 60 * 60) + (M1 * 60) + S1;
-                fim = (D2 - 1) * (24 * 60 * 60) + (H2 * 60 * 60) + (M2 * 60) + S2;
-
-                duracao = fim - inicio;
-
-                DF = duracao / (24 * 60 * 60);
-                resto = duracao % (24 * 60 * 60);
-                HF = resto / (60 * 60);
-                resto = resto % (60 * 60);
-                MF = resto / 60;
-                SF = resto % 60;
-
-                Console.WriteLine($"{DF} dia(s)");
-                Console.WriteLine($"{HF} hora(s)");
-                Console.WriteLine($"{MF} minuto(s)");
-                Console.WriteLine($"{SF} segundo(s)");
-            }
+            Console.WriteLine("Digite a nota do segundo semestre: ");
+        }
+        if (!double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out N2) || N2 < 0 || N2 > 50)
+        {
+            Console.WriteLine("Valor inválido, favor redigitar.");
+            return;
         }
 
-        Console.ReadLine();
+        else
+        {
+            NF = N1 + N2;
+
+            if (NF > 70)
+            {
+                Console.WriteLine($"Sua nota foi de {NF.ToString("F2", CultureInfo.InvariantCulture)}\nVocê foi aprovado!!!!!");
+            }
+
+            else if (NF < 70)
+            {
+                Console.WriteLine($"Sua nota foi de {NF.ToString("F2", CultureInfo.InvariantCulture)}\nVocê foi reprovado.");
+            }
+
+            Console.ReadLine();
+        }
+
     }
 }
+
