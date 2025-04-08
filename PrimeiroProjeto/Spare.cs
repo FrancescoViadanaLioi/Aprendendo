3bba82359421464fe6cbb0d868d85cc12ef1b84e
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-04-08 14:16:50 -0300

    Programa que faz reajuste salarial e faz etapas de verificação da entrada.

--- PrimeiroProjeto/Spare.cs
+++ PrimeiroProjeto/Spare.cs
@@ -3,22 +3,48 @@ using System.Globalization;
 
 class Program
 {
-    static void Main()
+    public static void Main()
     {
-       Console.Write("Qual é o raio da esfera? ");
-       bool sucesso1 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out double raio);
+        double salario = 0, reajuste = 0, salariof = 0;
 
-        if(!sucesso1 || raio<=0)
+        Console.Write("Qual é o seu salário? R$");
+        bool sucesso1 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out salario);
+
+        if (!sucesso1 || salario <= 0)
         {
-            Console.WriteLine("Valor inválido. Por favor, inserir um número maior que zero.");
+            Console.WriteLine("Valor inválido. Favor redigitar (somente números positivos são permitidos).");
             return;
         }
-        else
+        else if (salario <= 400.00)
         {
-            double vol = (4.0/3.0) * Math.PI * Math.Pow(raio, 3);
-            Console.WriteLine($"O volume de uma esfera de raio {raio.ToString("F2", CultureInfo.InvariantCulture)} é de {vol.ToString("F2", CultureInfo.InvariantCulture)}. ");
+            reajuste = 0.15;
+            salariof = salario + salario * reajuste;
         }
+        else if (salario > 400.00 && salario <= 800.00)
+        {
+            reajuste = 0.12;
+            salariof = salario + salario * reajuste;
+        }
+        else if (salario > 800.00 && salario <= 1200.00)
+        {
+            reajuste = 0.10;
+            salariof = salario + salario * reajuste;
+        }
+        else if (salario > 1200.00 && salario <= 2000.00)
+        {
+            reajuste = 0.07;
+            salariof = salario + salario * reajuste;
+        }
+        else if (salario > 2000.00)
+        {
+            reajuste = 0.04;
+            salariof = salario + salario * reajuste;
+        }
+
+        reajuste *= 100;
+
+        Console.WriteLine($"O salário de R${salario.ToString("F2", CultureInfo.InvariantCulture)} com um reajuste de {reajuste.ToString("F1",CultureInfo.InvariantCulture)}% terá como salario final R${salariof.ToString("F2", CultureInfo.InvariantCulture)}");
         Console.ReadLine();
     }
-    }
+}
 
