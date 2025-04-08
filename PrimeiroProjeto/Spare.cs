ad9aea383adbd10c9dae5e4fa438856b57a27f9d
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-04-08 15:05:26 -0300

    Código que verifica os dados de entrada e retorna o intervalo que o código está inserido, contendo sistema de validação de dados.

--- PrimeiroProjeto/Spare.cs
+++ PrimeiroProjeto/Spare.cs
@@ -5,46 +5,36 @@ class Program
 {
     public static void Main()
     {
-        double salario = 0, reajuste = 0, salariof = 0;
+        float NI;
+        string intervalo;
 
-        Console.Write("Qual é o seu salário? R$");
-        bool sucesso1 = double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out salario);
+        Console.Write("Digite um número: ");
+        bool sucesso = float.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out NI);
 
-        if (!sucesso1 || salario <= 0)
+        if (!sucesso)
         {
-            Console.WriteLine("Valor inválido. Favor redigitar (somente números positivos são permitidos).");
+            Console.WriteLine("Valor inválido");
             return;
         }
-        else if (salario <= 400.00)
+        else if (NI >= 0.00f && NI <= 25.00f)
         {
-            reajuste = 0.15;
-            salariof = salario + salario * reajuste;
+            intervalo = "está no intervalo [0;25]";
         }
-        else if (salario > 400.00 && salario <= 800.00)
+        else if (NI > 25.00f && NI <= 50.00f)
         {
-            reajuste = 0.12;
-            salariof = salario + salario * reajuste;
+            intervalo = "está no intervalo ]25;50]";
         }
-        else if (salario > 800.00 && salario <= 1200.00)
+        else if (NI > 50.00f && NI <= 100.00f)
         {
-            reajuste = 0.10;
-            salariof = salario + salario * reajuste;
+            intervalo = "está no intervalo ]50;100]";
         }
-        else if (salario > 1200.00 && salario <= 2000.00)
+        else
         {
-            reajuste = 0.07;
-            salariof = salario + salario * reajuste;
-        }
-        else if (salario > 2000.00)
-        {
-            reajuste = 0.04;
-            salariof = salario + salario * reajuste;
+            intervalo = "está fora do intervalo";
         }
 
-        reajuste *= 100;
-
-        Console.WriteLine($"O salário de R${salario.ToString("F2", CultureInfo.InvariantCulture)} com um reajuste de {reajuste.ToString("F1",CultureInfo.InvariantCulture)}% terá como salario final R${salariof.ToString("F2", CultureInfo.InvariantCulture)}");
-        Console.ReadLine();
+            Console.WriteLine($"O número {NI.ToString(CultureInfo.InvariantCulture)} {intervalo}.");
+            Console.ReadLine();
     }
 }
 
